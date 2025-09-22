a3ddd5d2c2b5b4c0969ebffb0ad7161270476fdc
MauroSezella <86243140+MauroSezella@users.noreply.github.com>
2025-09-22 15:46:35 -0300

    Update licencia de SMILE

--- src/App.java
+++ src/App.java
@@ -3,27 +3,29 @@ import IU.BayesianNetworkUI;
 
 public class App {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        new smile.License(
-                "SMILE LICENSE 171a4b7e 471987c8 0ba55192 " +
-                        "THIS IS AN ACADEMIC LICENSE AND CAN BE USED " +
-                        "SOLELY FOR ACADEMIC RESEARCH AND TEACHING, " +
-                        "AS DEFINED IN THE BAYESFUSION ACADEMIC " +
-                        "SOFTWARE LICENSING AGREEMENT. " +
-                        "Serial #: 4iktymxi89kq3mt69f56rpl2l " +
-                        "Issued for: Mauro Sezella (maurosezella@gmail.com) " +
-                        "Academic institution: Universidad Nacional de Santiago del Estero " +
-                        "Valid until: 2025-09-23 " +
-                        "Issued by BayesFusion activation server",
-                new byte[] {
-                        -103, -59, -60, -81, -100, -2, 116, -50, 24, 126, -54, 5, 99, -46, 122, -23,
-                        -83, -55, 42, -70, 62, 110, -61, 98, 31, 57, 66, 74, 27, 30, 84, -63,
-                        -48, 32, 17, 113, 17, -126, -17, -60, -89, 43, -102, -48, -56, 97, 71, 98,
-                        116, -122, 15, -120, 109, -42, 5, -124, -68, 80, 20, 8, -126, -65, 37, -3
-                });
+                new smile.License(
+                                "SMILE LICENSE a288bbf2 f22bc170 4d1da11e " +
+                                                "THIS IS AN ACADEMIC LICENSE AND CAN BE USED " +
+                                                "SOLELY FOR ACADEMIC RESEARCH AND TEACHING, " +
+                                                "AS DEFINED IN THE BAYESFUSION ACADEMIC " +
+                                                "SOFTWARE LICENSING AGREEMENT. " +
+                                                "Serial #: 5xscv33zrd0ue56fipab0z8wa " +
+                                                "Issued for: Mauro Sezella (maurosezella@gmail.com) " +
+                                                "Academic institution: Universidad Nacional de Santiago del Estero " +
+                                                "Valid until: 2026-03-26 " +
+                                                "Issued by BayesFusion activation server",
+                                new byte[] {
+                                                -73, -40, 23, -46, -48, 55, -73, 120, 22, -44, 127, 46, 75, 121, 66, 27,
+                                                43, -84, 37, -43, -33, -82, -77, 29, 105, -26, -10, 14, 11, -48, 3, 3,
+                                                78, 56, 23, -51, 119, -126, -13, -17, -111, -18, -73, -36, -107, 86,
+                                                -59, -106,
+                                                7, -99, 108, 9, -37, -31, -104, -88, -91, -105, -100, 67, -47, 100, 116,
+                                                -66
+                                });
 
-        SwingUtilities.invokeLater(() -> new BayesianNetworkUI());
+                SwingUtilities.invokeLater(() -> new BayesianNetworkUI());
 
-    }
+        }
 }
